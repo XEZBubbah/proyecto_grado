8b661a2af3fc506dd48afa092842adee2c034734
Jhoan Ortiz <63681573+RecursiveDeveloper@users.noreply.github.com>
2022-03-09 22:08:35 -0500

    Actualizacion esquema del modelo MongoDB
    Se incluyen los esquemas para almacenar informacion en las colecciones destinadas a la App Web y la App Movil

--- server/index.js
+++ server/index.js
@@ -12,9 +12,14 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended:true}));
 app.use(cors());
 app.use('/user', userRoutes);
 //Conexión base de datos mongoDB
-const CONNECTION_URL = 'mongodb+srv://jparra305:Bicicletas1@clusterciclorrutas.jp40z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const BD = 'ciclorrutaDB';
+const USER = 'jparra305';
+const PASS = 'Bicicletas1';
+const CONNECTION_URL = `mongodb+srv://${USER}:${PASS}@clusterciclorrutas.jp40z.mongodb.net/${BD}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(PORT, () => 
+        console.log(`Server running on port: ${PORT}`)
+    ))
     .catch((error) => console.log(error.message));
