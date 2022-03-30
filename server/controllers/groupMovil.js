96864e39c00898b87794f3ff24a8b9bd004a0643
Jhoan Ortiz <63681573+RecursiveDeveloper@users.noreply.github.com>
2022-03-29 23:54:46 -0500

    Rutas añadidas para la seccion de grupos
    fetchGroupMov => Listar grupos en general
    fetchUserGroupMov => Listar grupos asociados a un usuario
    getGroupMov => Ingresar a grupo determinado de un usuario

--- server/controllers/groupMovil.js
+++ server/controllers/groupMovil.js
@@ -44,6 +44,52 @@ exports.createGroup = async(req,res) => {
 	}
 }
 
+exports.fetchGroup = async(req,res) => {
+	try {
+        var groups = await Group.Grupos.find({});
+        console.log("Grupos");
+        console.log(groups);
+        //Filtrar documentos quitando informacion de campos sin interes para esta funcion
+        Object.keys(groups).map(key => {
+            groups[key].Contraseña_Grupo = null
+            groups[key].Descripcion = null,
+            groups[key].UAppMov_Id = null
+        });
+
+        res.status(200).json({result: groups});
+	} catch (error) {
+		res.status(500).json({message: "Algo salió mal durante la petición"});
+		console.log(error);
+	}
+}
+
+exports.fetchUserGroup = async(req,res) => {
+	const { Usuario } = req.body;
+	try {
+        const user = await User.UsuariosAppMovil.findOne({Usuario: Usuario});
+        if(!user) return res.status(400).json({ message: "No se encuentra un usuario relacionado"});
+        const user_Id = user.id;
+        console.log("Usuario Grupo");
+        console.log(user);
+
+		var existingGroups = await Group.Grupos.find({UAppMov_Id: user_Id});
+        console.log("Grupos existentes");
+        console.log(existingGroups);
+
+        //Filtrar documentos quitando informacion de campos sin interes para esta funcion
+        Object.keys(existingGroups).map(key => {
+            existingGroups[key].Contraseña_Grupo = null
+            existingGroups[key].Descripcion = null,
+            existingGroups[key].UAppMov_Id = null
+        });
+
+        res.status(200).json({result: existingGroups});
+	} catch (error) {
+		res.status(500).json({message: "Algo salió mal durante la petición"});
+		console.log(error);
+	}
+}
+
 exports.getGroup = async(req,res) => {
 	const {
         Nombre_Grupo, Contraseña_Grupo, Usuario
@@ -63,6 +109,9 @@ exports.getGroup = async(req,res) => {
         if(visibility == 'Privado' && Contraseña_Grupo.length == 0) {
             return res.status(400).json({ message: "No puede acceder a un grupo privado sin digitar la contraseña"});
         }else{
+            const isPasswordCorrect = await bcrypt.compare(Contraseña_Grupo, existingGroup.Contraseña_Grupo);
+            if(!isPasswordCorrect) return res.status(404).json({ message:"La contraseña no es correcta"});
+
             res.status(200).json({
                 result: {
                     Nombre_Grupo: existingGroup.Nombre_Grupo,
--- server/routes/groupMovil.js
+++ server/routes/groupMovil.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const createGroup = require("../controllers/groupMovil");
+const fetchGroup = require("../controllers/groupMovil");
+const fetchUserGroup = require("../controllers/groupMovil");
 const getGroup = require("../controllers/groupMovil");
 var router = express.Router();
 
 router.post('/createGroupMov', createGroup.createGroup);
-router.post('/getGroupMov', getGroup.getGroup);
+router.post('/fetchGroupMov', fetchGroup.fetchGroup);   //Listar grupos en general
+router.post('/fetchUserGroupMov', fetchUserGroup.fetchUserGroup); //Listar grupos asociados a un usuario
+router.post('/getGroupMov', getGroup.getGroup); //Ingresar a grupo determinado de un usuario
 
 module.exports = router;
