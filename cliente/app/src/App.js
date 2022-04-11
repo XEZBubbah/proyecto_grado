ae7d749c78cc6af0b34f54f9dd95cb843ec53927
CG-2407 <62751408+CG-2407@users.noreply.github.com>
2022-04-11 17:09:17 -0500

    Modificaciones visuales y dashboard
    Modificaciones visuales y creación de dashboard inicial.

--- cliente/app/src/App.js
+++ cliente/app/src/App.js
@@ -1,20 +1,21 @@
 import React  from "react";
-import Navbar from "./componentes/Navbar/Navbar";
-import { Container } from "@material-ui/core";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./componentes/Home/Home";
 import Auth from "./componentes/Auth/Auth";
+import Dashboard from "./componentes/Dashboard/Dashboard";
 
 const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
     return(
-        <BrowserRouter>
-        <Container maxWidth="lg">
-         <Navbar/>
-            <Routes>   
-                <Route path="/" exact element={<Home/>}/>
-                <Route path="/signin" exact element={<Auth/>}/>
+    <BrowserRouter>
+        <div>
+            <Routes>
+                <Route path="/" exact element={<Navigate to="/home"/>}/>
+                <Route path="/home" exact element={<Home/>}/>
+                <Route path="/dashboard" exact element={<Dashboard/>}/>
+                <Route path="/signin" exact element={user? <Navigate to="/home"/> : <Auth/>}/>
             </Routes>
-        </Container>
+        </div>
     </BrowserRouter>
     );
 
