6d698103a32494ca29dd9958f6f9a870fce2357f
Jhoan Ortiz <63681573+RecursiveDeveloper@users.noreply.github.com>
2022-04-12 22:57:56 -0500

    Version 1 almacenamiento de imagen de perfil

--- server/controllers/userMovil.js
+++ server/controllers/userMovil.js
@@ -4,7 +4,6 @@ const User = require("./../models/movilModel");
 const Grupo = require("./../models/movilModel");
 const Itinerario = require("./../models/movilModel");
 const secret = 'test';
-const multer = require('multer');
 
 exports.signin = async(req,res) => {
 	const { userName, password } =req.body;
@@ -24,30 +23,17 @@ exports.signin = async(req,res) => {
 	}
 }
 
-//define storage for the images
-const storage = multer.diskStorage({
-	//destination for files
-	destination: "uploadImages",
-	//add back the extension
-	filename: function (request, file, callback) {
-	callback(null, Date.now() + file.originalname);
-	},
-});
-//upload parameters for multer
-const upload = multer({
-	storage: storage,
-	limits: {fieldSize: 1024 * 1024 * 3,},
-});
 exports.signup = async(req,res) => {
 	const {email, password, confirmPassword, firstName, lastName, userName, birthDate, phone, avatar} = req.body;
 	try {
+		console.log("Request de la imagen");
+		console.log(req.file);
 		const existingUser = await User.UsuariosAppMovil.findOne({Correo:email, Usuario:userName});
 		if(existingUser) return res.status(400).json({message: "El usuario ya existe"});
 		//if(password !== confirmPassword) return res.status(200).json({ message: "Las contraseñas no coinciden"});
 		const hashedPassword = await bcrypt.hash(password, 12);
 		console.log(email, password, firstName);
 		
-		upload.single("avatar");
 		const result = await User.UsuariosAppMovil.create({
 			Nombre: firstName, 
 			Apellido: lastName,
@@ -56,7 +42,7 @@ exports.signup = async(req,res) => {
 			Celular: phone,
 			Correo: email, 
 			Contraseña: hashedPassword,
-			Avatar: avatar //avatar.filename
+			Avatar: avatar //req.file.avatar.buffer
 		});
 		res.status(200).json({result: result});
 	} catch (error) {
