e0b1d487235ba7a4b8cd8941467d5e94b17c7a75
CG-2407 <62751408+CG-2407@users.noreply.github.com>
2022-04-25 21:48:18 -0500

    Cartography component

--- cliente/app/src/componentes/Cartografía/Map.js
+++ cliente/app/src/componentes/Cartografía/Map.js
@@ -2,21 +2,33 @@ import React, { useEffect, useState, useMemo} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Box } from '@material-ui/core';
 import Title from '../Title/Title';
-import { GoogleMap, useLoadScript, Marker, KmlLayer} from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker, KmlLayer} from "@react-google-maps/api";
 
+const containerStyle = {
+    width: '100%',
+    height: '80vh'
+  };
+
+const center = {
+    lat:7.11392,
+    lng:-73.1198
+}
 const Map = () =>{
-    const {isLoaded} = useLoadScript({
-        googleMapsApiKey : "21dee9c2ff47493cb045541fc77d1b59",
-    })
-    const center = useMemo(() => ({lat: 7.11392,lng:-73.1198}),[])
-    if(!isLoaded) return <Container>NoMap</Container>
 
     return(
-        <Box sx={{height:1000}}>
-            <GoogleMap zoom={10} center={center} width='100%' height='100vh'>
-                <KmlLayer url="https://pastebin.com/raw/203B9ixP"/>
-            </GoogleMap>
-        </Box>
+    <Box>
+    <LoadScript
+        googleMapsApiKey="AIzaSyCsIRQS7oa0whj1gyDbKyYciTx8DZoBTlQ"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={15}
+      >
+        <KmlLayer url="https://pastebin.com/raw/203B9ixP"/>
+      </GoogleMap>
+    </LoadScript>
+    </Box>
     )
 }
 
