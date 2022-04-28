9d7bb4467b52007ecbcc3ca9fd37844fb9417096
Jhoan Ortiz <63681573+RecursiveDeveloper@users.noreply.github.com>
2022-04-27 20:40:39 -0500

    Mocha Testing setup V1
    Si se daña algo me avisan :D

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const dotenv = require('dotenv');
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const mongooseConnect = require('./models/mongoDBConnect');
 const cors = require("cors");
 const userAdminRoutes = require("./routes/userAdmin");
 const userMovilRoutes = require("./routes/userMovil");
@@ -10,8 +11,10 @@ const chatMovilRoutes = require("./routes/chatMovil");
 const reportMovilRoutes = require("./routes/reportesMovil");
 const reportAdminRoutes = require("./routes/reporteAdmin");
 const config = require("./cors.js");
-
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+dotenv.config({path: __dirname + '/.env'});
 
 app.use(bodyParser.json({limit: '30mb', extended:true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended:true}));
@@ -24,15 +27,7 @@ app.use('/groupM', groupMovilRoutes);
 app.use('/itineraryM', itineraryMovilRoutes);
 app.use('/chatM', chatMovilRoutes);
 
-//Conexión base de datos mongoDB
-const BD = 'ciclorrutaDB';
-const USER = 'jparra305';
-const PASS = 'Bicicletas1';
-const CONNECTION_URL = `mongodb+srv://${USER}:${PASS}@clusterciclorrutas.jp40z.mongodb.net/${BD}?retryWrites=true&w=majority`;
-const PORT = process.env.PORT || 5000;
+//Conexión a la base de datos MongoDB
+mongooseConnect.dbconnect(app,PORT);
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => 
-        console.log(`Server running on port: ${PORT}`)
-    ))
-    .catch((error) => console.log(error.message));
+module.exports = app;
